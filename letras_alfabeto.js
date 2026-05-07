995f5d3a2dcad3034c6ca37faf1512d950e63476
Isabel <albornozparedesisabel@gmail.com>
2026-05-06 20:31:45 -0500

    Create letras_alfabeto.js

---
+++ letras_alfabeto.js
@@ -0,0 +1,8 @@
+let letras_alfabeto = ["a", "b", "c", "d", “e”]
+for (let letra of letras_alfabeto) {
+console. log(letra);
+}
+console. log (letras_alfabeto. length);
+
+let longitud = letras alfabeto. length;
+console. log(longitud);

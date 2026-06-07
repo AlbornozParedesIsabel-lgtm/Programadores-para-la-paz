ffd964c5f9c65d85b8eba55d89dbd622ac931c48
Isabel <albornozparedesisabel@gmail.com>
2026-06-06 21:07:19 -0500

    Set up Express server with routes for registration

--- semana6/server.js
+++ semana6/server.js
@@ -1,1 +1,34 @@
+const express = require('express');
+const app = express();
 
+// Middleware obligatorio para que Express pueda entender formatos JSON en las peticiones
+app.use(express.json());
+
+// Ruta para la Pregunta 6 y 7
+app.post('/registro', (req, res) => {
+  const nombre = req.body.nombre;
+  const mensaje = req.body.mensaje;
+
+  res.json({
+    estado: "Datos recibidos",
+    nombre: nombre,
+    mensaje: mensaje
+  });
+});
+
+// Ruta para la Pregunta 8 (Reporte comunitario)
+app.post('/incidencia', (req, res) => {
+  const tipo = req.body.tipo;
+  const descripcion = req.body.descripcion;
+
+  res.json({
+    mensaje: "Incidencia registrada",
+    tipo: tipo,
+    descripcion: descripcion
+  });
+});
+
+// Inicialización del servidor en el puerto 3000
+app.listen(3000, () => {
+  console.log('Servidor ejecutándose en puerto 3000');
+});
